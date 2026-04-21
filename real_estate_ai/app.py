7ad11fa45fb8d7329c365cc128b50d1d39b36e2b
CherryMehta <cherry.mehta007@gmail.com>
2026-04-21 22:43:30 +0530

    Update app.py

--- real_estate_ai/app.py
+++ real_estate_ai/app.py
@@ -15,6 +15,8 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "http://13.204.14.38/"
+        ,
     ],
     allow_credentials=True,
     allow_methods=["*"],
